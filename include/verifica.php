d90061eb7dc08acd25631e66098d01da3b03d0cf
Suy <suyanne.gabriele@gmail.com>
2019-08-19 16:37:39 -0300

    Login Backend att

--- include/verifica.php
+++ include/verifica.php
@@ -3,27 +3,24 @@ include_once('conexao.php');
 
 if(empty($_POST['email']) || empty($_POST['senha'])) {
 header('Location: ../login.php');
-exit();
 }
 
 //  Verificar se as informações necessárias foram preenchidas
-$nome = mysqli_real_escape_string($con, $_POST['email']);
-$senha = mysqli_real_escape_string($con, $_POST['senha']);
+$email = mysqli_real_escape_string($con, $_POST['email']);
+$senha = md5(mysqli_real_escape_string($con, $_POST['senha']));
 
-$query = "SELECT id, email, senha FROM usuario WHERE email = '{$email}' AND senha = '{$senha}'";
+$query = "SELECT id, email, senha FROM usuario WHERE email = '$email' AND senha = '$senha'";
 
 $result = mysqli_query($con, $query);
 
 $row = mysqli_num_rows($result);
 
 if ($row == 1) {
-$_SESSION['email_existe'] = $email;
+$_SESSION['email'] = $email;
 header('Location: ../index.php');
-exit();
 } else {
 $_SESSION['nao_autenticado'] = true;
 header('Location: ../login.php');
-exit();
 }
 
 ?>
--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 
 include_once('include/conexao.php');
 
+
+
 ?> 
 <!DOCTYPE html>
 <html>
