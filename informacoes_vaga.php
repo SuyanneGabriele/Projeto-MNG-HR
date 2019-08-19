20a0a9432c5115705a0284459df52bb051bc7053
Suyanne <suyanne.gabriele@gmail.com>
2019-08-19 17:22:53 -0300

    commit das vagas

---
+++ informacoes_vaga.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+
+  <title>Informações da Vaga</title>
+
+  <?php
+  require_once('include/links.php')
+  ?>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+   <?php
+   require_once('include/navbar.php');
+   ?>
+
+   <!-- Content Wrapper -->
+   <div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Content -->
+    <div id="content">
+
+      <!-- Topbar -->
+
+      <?php
+      require_once('include/topbar.php')
+      ?>
+
+      <!-- End of Topbar -->
+      <!-- Inicio -->
+
+      <div class="container">
+            <h2>Oportunidades
+        <div class="card">
+          <div class="card-body">
+          <h4>Informações da vaga</h4>
+          <hr>
+          <h1><span style="color: black;"><strong>Arquiteto de Software (DevOps)</span></strong></h1>
+          <span style="font-size: 20px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+          <a href="vagas.php" style="text-decoration: underline; font-size: 17px; color: rgba(0,0,0,.5);">Voltar para a lista de vagas</a>
+          </div>
+        </div>
+      </div>
+    <!-- Fim -->
+</div>
+
+
+
+
+<!-- Footer -->
+<?php
+require_once('include/footer.php')
+?>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+  <i class="fas fa-angle-up"></i>
+</a>
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="js/demo/chart-area-demo.js"></script>
+<script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
--- vagas.php
+++ vagas.php
@@ -3,80 +3,156 @@
 
 <head>
 
-  <meta charset="utf-8">
+	<meta charset="utf-8">
 
-  <title>Vagas</title>
+	<title>Vagas</title>
 
-  <?php
-  require_once('include/links.php')
-  ?>
+	<?php
+	require_once('include/links.php')
+	?>
 
 </head>
 
 <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
+	<!-- Page Wrapper -->
+	<div id="wrapper">
 
-   <?php
-   require_once('include/navbar.php');
-   ?>
+		<?php
+		require_once('include/navbar.php');
+		?>
 
-   <!-- Content Wrapper -->
-   <div id="content-wrapper" class="d-flex flex-column">
+		<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
 
-    <!-- Content -->
-    <div id="content">
+			<!-- Content -->
+			<div id="content">
 
-      <!-- Topbar -->
+				<!-- Topbar -->
 
-      <?php
-      require_once('include/topbar.php')
-      ?>
+				<?php
+				require_once('include/topbar.php')
+				?>
 
-      <!-- End of Topbar -->
-      <!-- Inicio -->
-      <p>Oportunidades</p>
-    <!-- Fim -->
-</div>
+				<!-- End of Topbar -->
+				<!-- Inicio -->
+
+				<div class="container">
+					<!-- Outer Row -->
+					<div class="row justify-content-center">
+						<div class="col-xl-10 col-lg-12 col-md-9">
+							<div class="card o-hidden border-0 shadow-lg my-5">
+								<div class="card-body p-0">
+
+									<!-- Nested Row within Card Body -->
+									<div class="row">
+										<div class="col-lg-12 col">
+											<div class="p-5">
+												<div class="text-center">
+													<h1 class="h4 text-gray-900 mb-4"><strong>Oportunidades</strong></h1>
+												</div>
+												<hr>
+												<form class="form-inline md-form form-sm">
+													<input class="form-control form-control-sm mr-3 w-75 col-11" type="text" placeholder="Search"
+													aria-label="Search">
+													<i class="fas fa fa-search"></i>
+												</form>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Arquiteto de Software (DevOps)</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+													</div>
+												</a>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Arquiteto de Software (Tecnologia)</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Itu(SP)</span>
+													</div>
+												</a>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Dev Java (Manutenção Performance Corporativa e Tecnologia)</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+													</div>
+												</a>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Dev Java (DevOps)</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+													</div>
+												</a>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Analista de Sistemas Java (DevOps)</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+													</div>
+												</a>
+												<hr>
+												<a href="" style="text-decoration: none;">	
+													<div class="topnav">
+														<button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button>
+														<span style="color: black;">Analista de Inteligência de Mercado</span><br>
+														<span style="font-size: 15px; color: rgba(0,0,0,.5);">Blumenau(SC)</span>
+													</div>
+												</a>
+												<hr>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Fim -->
+			</div>
 
 
 
+			<!-- Footer -->
+			<?php
+			require_once('include/footer.php')
+			?>
+			<!-- End of Footer -->
 
-<!-- Footer -->
-<?php
-require_once('include/footer.php')
-?>
-<!-- End of Footer -->
+		</div>
+		<!-- End of Content Wrapper -->
 
-</div>
-<!-- End of Content Wrapper -->
+	</div>
+	<!-- End of Page Wrapper -->
 
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-  <i class="fas fa-angle-up"></i>
-</a>
+	<!-- Scroll to Top Button-->
+	<a class="scroll-to-top rounded" href="#page-top">
+		<i class="fas fa-angle-up"></i>
+	</a>
 
 
-<!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Bootstrap core JavaScript-->
+	<script src="vendor/jquery/jquery.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+	<!-- Core plugin JavaScript-->
+	<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-<!-- Custom scripts for all pages-->
-<script src="js/sb-admin-2.min.js"></script>
+	<!-- Custom scripts for all pages-->
+	<script src="js/sb-admin-2.min.js"></script>
 
-<!-- Page level plugins -->
-<script src="vendor/chart.js/Chart.min.js"></script>
+	<!-- Page level plugins -->
+	<script src="vendor/chart.js/Chart.min.js"></script>
 
-<!-- Page level custom scripts -->
-<script src="js/demo/chart-area-demo.js"></script>
-<script src="js/demo/chart-pie-demo.js"></script>
+	<!-- Page level custom scripts -->
+	<script src="js/demo/chart-area-demo.js"></script>
+	<script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
