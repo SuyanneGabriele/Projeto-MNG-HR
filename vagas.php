3b03a43f64e05e049c395b7070b49f4d9af41c7b
Suy <suyanne.gabriele@gmail.com>
2019-09-16 17:13:16 -0300

    perfil pronto

--- vagas.php
+++ vagas.php
@@ -36,9 +36,9 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 </head>
 <style type="text/css">
 
-.oculta_pesquisa{
-	display: none;
-}
+	.oculta_pesquisa{
+		display: none;
+	}
 
 </style>
 <body id="page-top">
@@ -80,71 +80,76 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 												</div>
 												<hr>
 												<form class="form-inline md-form form-sm mb-3 mr-2 w-100 Row" method="POST">
+
+													<!-- -------- AVISOS ---------- -->
 													<?php if ($_SESSION['add_sucesso']) { ?>
-													<div class="col-12">
-														<div class="alert alert-success" role="alert">
-															Vaga adicionada com sucesso!
+														<div class="col-12">
+															<div class="alert alert-success" role="alert">
+																Vaga adicionada com sucesso!
+															</div>
 														</div>
-													</div>
 													<?php }; UNSET($_SESSION['add_sucesso']); ?>
-													<input class="form-control form-control-sm ml-3 mr-3 w-75 col-11" type="text" id="barra_pesquisa" name="pesquisar" placeholder="Pesquisar" aria-label="Search">
+
+													<!-- ---------	BARRA PESQUISAR ---------- -->
+													<input class="form-control form-control-sm ml-3 mr-3 w-75 col-11" type="search" id="barra_pesquisa" name="pesquisar" placeholder="Pesquisar" aria-label="Search">
 													<i class="fas fa fa-search"></i>
-													<?php
-													if ($pesquisar = $_POST['pesquisar'])
-														$sql3 = "SELECT * FROM vagas WHERE nome_vaga LIKE '%$pesquisar%'";
-													$result3 = mysqli_query($con, $sql3);
-													while ($rows_vagas = mysqli_fetch_array($result3)){ ?>
 												</form>
-												<div class="topnav">
-													<a href="informacoes_vaga.php" style="text-decoration: none;">
-														<table class="w-100">
-															<tbody>
-																<tr style="color: black;"><td><?php echo utf8_encode($rows_vagas['nome_vaga']); ?></td></tr>
-																<td style="font-size: 15px; color: rgba(0,0,0,.5);"><?php echo utf8_encode($rows_vagas['cidade']); ?></td>
-																<td><button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button></td>
-																<tr><hr></tr>
-															</tbody>
-														</table>
-													</a>
-												</div>
-												<?php } ?>
-
 												<?php
-												if ($num >0) { ?>
-												<div class="topnav w-100 mt-3 d" id="oculta_div">
-
-													<?php do{ ?>
-													<div class="topnav mr-0">
-														<a href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" name="infoVaga" style="text-decoration: none;">
+												if ($pesquisar = $_POST['pesquisar'])
+													$sql3 = "SELECT * FROM vagas WHERE nome_vaga LIKE '%$pesquisar%'";
+												$result3 = mysqli_query($con, $sql3);
+												
+												while ($rows_vagas = mysqli_fetch_array($result3)){ ?>
+													<div class="topnav">
+														<a href="informacoes_vaga.php" style="text-decoration: none;">
 															<table class="w-100">
 																<tbody>
-																	<tr style="color: black;"><td><?php echo utf8_encode($vaga['nome_vaga']); ?></td></tr>
-																	<td style="font-size: 15px; color: rgba(0,0,0,.5);"><?php echo utf8_encode($vaga['cidade']); ?></td>
-																	<td><a type="button" name="infoVaga" href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" class="btn btn-primary" style="float: right;">Ver detalhes</a></td>
+																	<tr style="color: black;"><td><?php echo utf8_encode($rows_vagas['nome_vaga']); ?></td></tr>
+																	<td style="font-size: 15px; color: rgba(0,0,0,.5);"><?php echo utf8_encode($rows_vagas['cidade']); ?></td>
+																	<td><button type="button" class="btn btn-primary" style="float: right;">Ver detalhes</button></td>
 																	<tr><hr></tr>
 																</tbody>
 															</table>
 														</a>
-														<?php } while($vaga = $result->fetch_assoc()); ?>
 													</div>
-
-												</div>
 												<?php } ?>
 
-												<!-- Pagination -->
-												<div class="row w-100 mt-3">
-													<div class="col-12 col-md-6">
-														<nav>
-															<ul class="pagination">
-																<li class="page-item"><a class="page-link" href="vagas.php?pagina=0">Anterior</a></li>
+													<?php
+													if ($num >0) { ?>
+														<div class="topnav w-100 mt-3 d" id="oculta_div">
 
-																<?php
-																for ($i = 0; $i < $num_pagina; $i++) {
-																	$estilo = "";
-																	if ($pagina == $i)
-																		$estilo = "\"active\"";
-																	?>
-																	<li class="page-item <?php echo $estilo; ?>"><a class="page-link" href="vagas.php?pagina=<?php echo $i; ?>"><?php echo $i+1;?></a></li>
+															<?php do{ ?>
+																<div class="topnav mr-0">
+																	<a href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" name="infoVaga" style="text-decoration: none;">
+																		<table class="w-100">
+																			<tbody>
+																				<tr style="color: black;"><td><?php echo utf8_encode($vaga['nome_vaga']); ?></td></tr>
+																				<td style="font-size: 15px; color: rgba(0,0,0,.5);"><?php echo utf8_encode($vaga['cidade']); ?></td>
+																				<td><a type="button" name="infoVaga" href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" class="btn btn-primary" style="float: right;">Ver detalhes</a></td>
+																				<tr><hr></tr>
+																			</tbody>
+																		</table>
+																	</a>
+																<?php } while($vaga = $result->fetch_assoc()); ?>
+															</div>
+
+														</div>
+													<?php } ?>
+
+													<!-- Pagination -->
+													<div class="row w-100 mt-3">
+														<div class="col-12 col-md-6">
+															<nav>
+																<ul class="pagination">
+																	<li class="page-item"><a class="page-link" href="vagas.php?pagina=0">Anterior</a></li>
+
+																	<?php
+																	for ($i = 0; $i < $num_pagina; $i++) {
+																		$estilo = "";
+																		if ($pagina == $i)
+																			$estilo = "\"active\"";
+																		?>
+																		<li class="page-item <?php echo $estilo; ?>"><a class="page-link" href="vagas.php?pagina=<?php echo $i; ?>"><?php echo $i+1;?></a></li>
 
 																	<?php } ?>
 																	<li class="page-item"><a class="page-link" href="vagas.php?pagina=<?php echo $num_pagina-1; ?>">Próximo</a></li>
@@ -177,9 +182,16 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 
 <script type="text/javascript">
 
-	$("#barra_pesquisa").keydown(function(){
-		$("#oculta_div").addClass("d-none");
+	// $("#barra_pesquisa").keydown(function(){
+	// 	$("#oculta_div").addClass("d-none");
+	// });
+
+	$( "#pesquisar" ).keypress(function( event ) {   
+		if ( event.which == 13 ) {
+			alert('enter pressionado');  
+		}
 	});
+
 
 </script>
 
