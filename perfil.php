b8843e6bf588d9557fed5e6104fed1099be7f6ed
Suy <suyanne.gabriele@gmail.com>
2019-09-17 14:00:14 -0300

    att

--- perfil.php
+++ perfil.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION)){
 
 $email = $_SESSION['email'];
 
-
 $sql_perfil = "SELECT * FROM `cadastro_perfil` where email = '$email'"; 
 $result_perfil = mysqli_query($con, $sql_perfil);
 $info_perfil = $result_perfil->fetch_assoc();
@@ -23,8 +22,7 @@ $num_profissao = mysqli_num_rows($result_profissao);
 $sql_escolaridade = "SELECT * FROM escolaridade WHERE fk_usuario = '$id'";
 $result_escolaridade = mysqli_query($con, $sql_escolaridade);
 $num_escolaridade = mysqli_num_rows($result_escolaridade);   
-
-
+	
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -215,14 +213,11 @@ $num_escolaridade = mysqli_num_rows($result_escolaridade);
 
 
 
-	<script src="perfil/js/aos.js"></script>
-	<script src="perfil/scripts/main.js"></script>
-	<script src="perfil/js/core/popper.min.js"></script>
-	<script src="perfil/js/now-ui-kit.js?v=1.1.0"></script>
-	<script src="perfil/js/core/bootstrap.min.js"></script>
-	<script src="perfil/js/core/jquery.3.2.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="perfil/js/core/jquery.3.2.1.min.js"></script>
+<script src="perfil/js/core/popper.min.js"></script>
+<script src="perfil/js/core/bootstrap.min.js"></script>
+<script src="perfil/js/now-ui-kit.js?v=1.1.0"></script>
+<script src="perfil/js/aos.js"></script>
+<script src="perfil/scripts/main.js"></script>
 </body>
 </html>
--- vagas.php
+++ vagas.php
@@ -36,21 +36,9 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 </head>
 <style type="text/css">
 
-<<<<<<< HEAD
 	.oculta_pesquisa{
 		display: none;
 	}
-=======
-<<<<<<< HEAD
-	.oculta_pesquisa{
-		display: none;
-	}
-=======
-.oculta_pesquisa{
-	display: none;
-}
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
->>>>>>> e1a3863b840c27f282bccf17e855f0b1361563c9
 
 </style>
 <body id="page-top">
@@ -92,8 +80,6 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 												</div>
 												<hr>
 												<form class="form-inline md-form form-sm mb-3 mr-2 w-100 Row" method="POST">
-
-													<!-- -------- AVISOS ---------- -->
 													<?php if ($_SESSION['add_sucesso']) { ?>
 														<div class="col-12">
 															<div class="alert alert-success" role="alert">
@@ -101,42 +87,16 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 															</div>
 														</div>
 													<?php }; UNSET($_SESSION['add_sucesso']); ?>
-
-													<!-- ---------	BARRA PESQUISAR ---------- -->
-													<input class="form-control form-control-sm ml-3 mr-3 w-75 col-11" type="search" id="barra_pesquisa" name="pesquisar" placeholder="Pesquisar" aria-label="Search">
+													<input class="form-control form-control-sm ml-3 mr-3 w-75 col-11" type="text" id="barra_pesquisa" name="pesquisar" placeholder="Pesquisar" aria-label="Search">
 													<i class="fas fa fa-search"></i>
-<<<<<<< HEAD
-=======
 													<?php
 													if ($pesquisar = $_POST['pesquisar'])
 														$sql3 = "SELECT * FROM vagas WHERE nome_vaga LIKE '%$pesquisar%'";
 													$result3 = mysqli_query($con, $sql3);
 													while ($rows_vagas = mysqli_fetch_array($result3)){ ?>
-<<<<<<< HEAD
 													</form>
 													<div class="topnav">
 														<a href="informacoes_vaga.php" style="text-decoration: none;">
-=======
->>>>>>> e1a3863b840c27f282bccf17e855f0b1361563c9
-												</form>
-												<?php
-<<<<<<< HEAD
-												if ($pesquisar = $_POST['pesquisar'])
-													$sql3 = "SELECT * FROM vagas WHERE nome_vaga LIKE '%$pesquisar%'";
-												$result3 = mysqli_query($con, $sql3);
-												
-												while ($rows_vagas = mysqli_fetch_array($result3)){ ?>
-													<div class="topnav">
-														<a href="informacoes_vaga.php" style="text-decoration: none;">
-=======
-												if ($num >0) { ?>
-												<div class="topnav w-100 mt-3 d" id="oculta_div">
-
-													<?php do{ ?>
-													<div class="topnav mr-0">
-														<a href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" name="infoVaga" style="text-decoration: none;">
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
->>>>>>> e1a3863b840c27f282bccf17e855f0b1361563c9
 															<table class="w-100">
 																<tbody>
 																	<tr style="color: black;"><td><?php echo utf8_encode($rows_vagas['nome_vaga']); ?></td></tr>
@@ -146,9 +106,6 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 																</tbody>
 															</table>
 														</a>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 													</div>
 												<?php } ?>
 
@@ -172,49 +129,23 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 														</div>
 
 													</div>
-														<?php } while($vaga = $result->fetch_assoc()); ?>
->>>>>>> e1a3863b840c27f282bccf17e855f0b1361563c9
-													</div>
 												<?php } ?>
 
-													<?php
-													if ($num >0) { ?>
-														<div class="topnav w-100 mt-3 d" id="oculta_div">
+												<!-- Pagination -->
+												<div class="row w-100 mt-3">
+													<div class="col-12 col-md-6">
+														<nav>
+															<ul class="pagination">
+																<li class="page-item"><a class="page-link" href="vagas.php?pagina=0">Anterior</a></li>
 
-															<?php do{ ?>
-																<div class="topnav mr-0">
-																	<a href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" name="infoVaga" style="text-decoration: none;">
-																		<table class="w-100">
-																			<tbody>
-																				<tr style="color: black;"><td><?php echo utf8_encode($vaga['nome_vaga']); ?></td></tr>
-																				<td style="font-size: 15px; color: rgba(0,0,0,.5);"><?php echo utf8_encode($vaga['cidade']); ?></td>
-																				<td><a type="button" name="infoVaga" href="informacoes_vaga.php?infoVaga=<?php echo utf8_encode($vaga['id']); ?>" class="btn btn-primary" style="float: right;">Ver detalhes</a></td>
-																				<tr><hr></tr>
-																			</tbody>
-																		</table>
-																	</a>
-																<?php } while($vaga = $result->fetch_assoc()); ?>
-															</div>
+																<?php
+																for ($i = 0; $i < $num_pagina; $i++) {
+																	$estilo = "";
+																	if ($pagina == $i)
+																		$estilo = "\"active\"";
+																	?>
+																	<li class="page-item <?php echo $estilo; ?>"><a class="page-link" href="vagas.php?pagina=<?php echo $i; ?>"><?php echo $i+1;?></a></li>
 
-														</div>
-													<?php } ?>
-
-													<!-- Pagination -->
-													<div class="row w-100 mt-3">
-														<div class="col-12 col-md-6">
-															<nav>
-																<ul class="pagination">
-																	<li class="page-item"><a class="page-link" href="vagas.php?pagina=0">Anterior</a></li>
-
-																	<?php
-																	for ($i = 0; $i < $num_pagina; $i++) {
-																		$estilo = "";
-																		if ($pagina == $i)
-																			$estilo = "\"active\"";
-																		?>
-																		<li class="page-item <?php echo $estilo; ?>"><a class="page-link" href="vagas.php?pagina=<?php echo $i; ?>"><?php echo $i+1;?></a></li>
-
-<<<<<<< HEAD
 																<?php } ?>
 																<li class="page-item"><a class="page-link" href="vagas.php?pagina=<?php echo $num_pagina-1; ?>">Próximo</a></li>
 															</ul>
@@ -222,32 +153,18 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 													</div>
 													<div class="col-12 col-md-6 p-0">
 														<a href="addvaga.php" class="btn btn-info float-right mt-2" style="margin-right: -12px">+ Adicionar vaga</a>
-=======
-																	<?php } ?>
-																	<li class="page-item"><a class="page-link" href="vagas.php?pagina=<?php echo $num_pagina-1; ?>">Próximo</a></li>
-																</ul>
-															</nav>
-														</div>
-														<div class="col-12 col-md-6 p-0">
-															<a href="addvaga.php" class="btn btn-info float-right mt-2" style="margin-right: -12px">+ Adicionar vaga</a>
-														</div>
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
 													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-
 								</div>
+
 							</div>
 						</div>
 					</div>
-
 				</div>
-<<<<<<< HEAD
 
-=======
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
 			</div>
 		</div>
 	</div>
@@ -256,23 +173,13 @@ $num_pagina = ceil($num_total/$itens_por_pagina);
 </div>
 </div>
 </div>
-<<<<<<< HEAD
 </div>
-=======
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
 
 <script type="text/javascript">
 
-	// $("#barra_pesquisa").keydown(function(){
-	// 	$("#oculta_div").addClass("d-none");
-	// });
-
-	$( "#pesquisar" ).keypress(function( event ) {   
-		if ( event.which == 13 ) {
-			alert('enter pressionado');  
-		}
+	$("#barra_pesquisa").keydown(function(){
+		$("#oculta_div").addClass("d-none");
 	});
-
 
 </script>
 
@@ -283,8 +190,4 @@ require_once('include/footer.php')
 <!-- End of Footer -->
 
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> 7cf0257bfba859cebdcda1b6f3f060684d35f80d
