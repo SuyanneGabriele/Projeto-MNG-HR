9091f026587e667244ec158e140a2973d24cd387
João <joao.preis1999@gmail.com>
2019-09-27 15:23:30 -0300

    att

--- include/registrobackend.php
+++ include/registrobackend.php
@@ -34,7 +34,15 @@ $id_usuario_criado = mysqli_insert_id($con);
 $sql_perfil = "INSERT INTO cadastro_perfil (`id`, `fk_perfil`, `nome`, `sobre`, `idade`, `email`, `telefone`, `endereco`, `cidade`, `estado`, `idiomas`, `nome_foto`) VALUES (DEFAULT, '$id_usuario_criado', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);";
 $result_perfil = mysqli_query($con, $sql_perfil);
 
+$sql_perfil2 = "INSERT INTO escolaridade (`id`, `fk_usuario`, `nome_faculdade`, `curso`, `data_escolaridade`, `ensino_superior`) VALUES (DEFAULT, '$id_usuario_criado', NULL, NULL, NULL, NULL);";
+$result_perfil2 = mysqli_query($con, $sql_perfil2);
 
+$sql_perfil3 = "INSERT INTO escolaridade2 (`id`, `fk_usuario`, `nome_medio`, `data_escolaridade_medio`, `ensino_medio`, `nome_fundamental`, `data_escolaridade_fundamental`, `ensino_fundamental`) VALUES (DEFAULT, '$id_usuario_criado', NULL, NULL, NULL, NULL, NULL, NULL);";
+$result_perfil3 = mysqli_query($con, $sql_perfil3);
+
+
+$sql_perfil4 = "INSERT INTO exp_profissional (`id`, `fk_usuario`, `profissao`, `sobre_profissao`, `data_profissao`, `cargo`) VALUES (DEFAULT, '$id_usuario_criado', NULL, NULL, NULL, NULL);";
+$result_perfil4 = mysqli_query($con, $sql_perfil4);
 }
 
 $con->close();
