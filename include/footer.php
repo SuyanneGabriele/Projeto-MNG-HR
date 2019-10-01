281bce011814597a5757f95b0639a487c2f9842f
Suyanne <suyanne.gabriele@gmail.com>
2019-10-01 15:28:04 -0300

    eu estou prestes a desistir de minha vida

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,48 @@
-    <footer class="sticky-footer bg-light shadow">
-      <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-          <span><strong>&copy; Feito por Management Human Resources </strong></span>
-        </div>
-      </div>
-    </footer>
+
+      <!-- Footer -->
+<footer style="box-shadow: 0px 1px 12px 1px rgb(0,0,0,.2);" class="page-footer font-small special-color-dark pt-4 mt-5">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fa fa-google-plus fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fa fa-dribbble fa-2x" aria-hidden="true"></i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019
+    Management Human Resources
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
