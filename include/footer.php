81810a61ccc5199552f96851ccfb5325ad7df6f9
João <joao.preis1999@gmail.com>
2019-10-01 15:47:28 -0300

    aa

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 
-      <!-- Footer -->
+<!-- Footer -->
 <footer style="box-shadow: 0px 1px 12px 1px rgb(0,0,0,.2);" class="page-footer font-small special-color-dark pt-4 mt-5">
 
   <!-- Footer Elements -->
@@ -8,27 +8,27 @@
     <!-- Social buttons -->
     <ul class="list-unstyled list-inline text-center">
       <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
+        <a href="https://facebook.com/login?lang=pt" class="btn-floating btn-fb mx-1">
           <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
         </a>
       </li>
       <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
+        <a href="https://twitter.com/login?lang=pt" class="btn-floating btn-tw mx-1" style="color: rgb(29,161,242);">
           <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
         </a>
       </li>
       <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
+        <a href="https://aboutme.google.com/u/0/?referer=inledin" class="btn-floating btn-gplus mx-1" style="color: red;">
           <i class="fa fa-google-plus fa-2x" aria-hidden="true"></i>
         </a>
       </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
+      <li class="list-inline-item" style="background-color: rgb(0,102,153);">
+        <a href="https://linkedin.com/login?lang=pt" class="btn-floating btn-li mx-1" style="color: white;">
           <i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
         </a>
       </li>
       <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
+        <a href="https://dribbble.com/login?lang=pt" class="btn-floating btn-dribbble mx-1" style="color: rgb(178,33,90);">
           <i class="fa fa-dribbble fa-2x" aria-hidden="true"></i>
         </a>
       </li>
